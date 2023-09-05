8ef848d934d1d3627f512dc70adf024b56ca518f
Michal Radomski <michalra@op.pl>
2023-09-05 03:13:11 +0200

    Course - commit 04 - course done

--- 01 Async JS/index.ts
+++ 01 Async JS/index.ts
@@ -1,4 +1,4 @@
-// //* Callback
+//* 1. Callback
 // function boilWater(clb: TimerHandler): void {
 //   console.log("Gotowanie wody...");
 //   setTimeout(clb, 1000);
@@ -12,7 +12,7 @@
 //   setTimeout(clb, 500);
 // }
 
-//@ Promise Hell
+//@ Callback Hell
 // boilWater(() => {
 //   console.log("Woda zagotowana");
 //   makeTea(() => {
@@ -23,7 +23,7 @@
 //   });
 // });
 
-//* Promise
+//* 2. Promise
 // const promise = new Promise((resolve, reject) => {
 //   setTimeout(() => {
 //     if (true) {
@@ -60,18 +60,111 @@ function waitingForGoodTemperature(): Promise<void> {
   });
 }
 
-boilWater()
-  .then(() => {
-    console.log("Woda zagotowana");
-    return makeTea();
-  })
-  .then(() => {
-    console.log("Herbata zaparzona");
-    return waitingForGoodTemperature();
-  })
-  .then(() => {
-    console.log("Temperatura Ok, można pić...");
-  })
-  .catch((err) => {
-    console.error("Coś poszło nie tak...", err);
+// boilWater()
+//   .then(() => {
+//     console.log("Woda zagotowana");
+//     return makeTea();
+//   })
+//   .then(() => {
+//     console.log("Herbata zaparzona");
+//     return waitingForGoodTemperature();
+//   })
+//   .then(() => {
+//     console.log("Temperatura Ok, można pić...");
+//   })
+//   .catch((err) => {
+//     console.error("Coś poszło nie tak...", err);
+//   });
+
+//* 3. Async/Await (+ IIFE)
+// (async () => {
+//   await boilWater();
+//   console.log("Woda zagotowana");
+//   await makeTea();
+//   console.log("Herbata zaparzona");
+//   await waitingForGoodTemperature();
+//   console.log("Temperatura Ok, można pić...");
+// })();
+
+//* Exercises -> V1
+// function doMyJob(hours: number, clb: Function) {
+//   if (hours > 8) {
+//     clb(new Error("Za długie godziny pracy"));
+//   } else {
+//     setTimeout(() => {
+//       clb(null);
+//     }, hours * 1000);
+//   }
+// }
+
+// function pay(clb: Function) {
+//   clb(null);
+// }
+
+// doMyJob(5, (err: Error) => {
+//   if (err === null) {
+//     console.log("Zadanie skończone");
+//     pay(() => {
+//       console.log("Zapłata poszła");
+//     });
+//   } else {
+//     console.log("Błąd:", err);
+//   }
+// });
+
+//* Exercises -> V2
+// function doMyJob(hours: number) {
+//   return new Promise<void>((resolve, reject) => {
+//     if (hours > 8) {
+//       reject(new Error("Za długie godziny pracy"));
+//     } else {
+//       setTimeout(() => {
+//         resolve();
+//       }, hours * 1000);
+//     }
+//   });
+// }
+
+// function pay() {
+//   return new Promise<void>((resolve, _reject) => resolve());
+// }
+
+// doMyJob(5)
+//   .then(() => {
+//     console.log("Zadanie skończone");
+//     return pay();
+//   })
+//   .then(() => {
+//     console.log("Wypłata poszła");
+//   })
+//   .catch((err) => {
+//     console.log("Bład:", err);
+//   });
+
+//* Exercises -> V3
+function doMyJob(hours: number) {
+  return new Promise<void>((resolve, reject) => {
+    if (hours > 8) {
+      reject(new Error("Za długie godziny pracy"));
+    } else {
+      setTimeout(() => {
+        resolve();
+      }, hours * 1000);
+    }
   });
+}
+
+function pay() {
+  return new Promise<void>((resolve, _reject) => resolve());
+}
+
+(async () => {
+  await doMyJob(7);
+  console.log("Zadanie skończone");
+  await pay();
+  console.log("Wypłata poszła");
+  try {
+  } catch (error) {
+    console.log("Bład:", error);
+  }
+})();
