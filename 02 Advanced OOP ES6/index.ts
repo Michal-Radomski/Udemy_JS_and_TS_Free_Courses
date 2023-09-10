7d83363487a1c7524f50e9986ed4a94521191245
Michal Radomski <michalra@op.pl>
2023-09-10 02:55:52 +0200

    Course - commit 02

--- 02 Advanced OOP ES6/index.ts
+++ 02 Advanced OOP ES6/index.ts
@@ -1,1 +1,12 @@
-console.log("test");
+const customerName = "John";
+const accountBalance = 2000;
+const isValid = true;
+const object: null = null;
+let test: undefined;
+console.log({ test }, typeof test);
+console.log("object:", object, typeof object);
+
+let x = "111";
+let y = 123;
+// @ts-ignore
+console.log("x == y, x === y:", x == y, x === y);
