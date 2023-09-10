9bde1e9f107a78d9c8cfd857e032a30286f450f4
Michal Radomski <michalra@op.pl>
2023-09-10 21:39:31 +0200

    Course - commit 04

--- 02 Advanced OOP ES6/index.ts
+++ 02 Advanced OOP ES6/index.ts
@@ -24,12 +24,55 @@
 // console.log(`age = ${age}  age1 = ${age1}`);
 
 //* Reference Types [Array, Object and Function]
-let info = {
-  Name: "Abc",
-  Age: 10,
-};
-console.log(`Name : ${info.Name} Age : ${info.Age}`);
+// let info = {
+//   Name: "Abc",
+//   Age: 10,
+// };
+// console.log(`Name : ${info.Name} Age : ${info.Age}`);
 
-let info1 = info;
-info1.Age = 14; // Change the Age of original object
-console.log(`Name : ${info.Name} Age : ${info.Age}`);
+// let info1 = info;
+// info1.Age = 14; // Change the Age of original object
+// console.log(`Name : ${info.Name} Age : ${info.Age}`);
+
+// Function constructor
+const sum = new Function("a", "b", "return a + b");
+console.log("sum(2, 6):", sum(2, 6)); // Expected output: 8
+
+// Date constructor
+const date1 = new Date("December 17, 1995 03:24:00"); // Sun Dec 17 1995 03:24:00 GMT...
+const date2 = new Date("1995-12-17T03:24:00"); // Sun Dec 17 1995 03:24:00 GMT...
+console.log("date1 === date2:", date1 === date2); // Expected output: false
+// @ts-ignore
+console.log("date1 - date2:", date1 - date2); // Expected output: 0
+
+const student = {
+  name: "Bob",
+  score: 80,
+};
+const s1 = new Object() as { name: string; score: number };
+s1.name = "Bob";
+s1.score = 80;
+console.log("student.name:", student.name);
+console.log("s1.name:", s1.name);
+
+const studentNames = ["John", "Bob", "Harry"];
+const sn = new Array("John", "Bob", "Harry");
+console.log("studentNames[0]:", studentNames[0]);
+console.log("sn[0]:", sn[0]);
+
+function hello(name: string) {
+  return "Hello " + name;
+}
+console.log("hello('Michal'):", hello("Michal"));
+const myFunc = new Function("name", "return 'Hello '+name");
+console.log('myFunc("Michal"):', myFunc("Michal"));
+
+const regEx = /\d+/ as RegExp;
+console.log("regEx:", regEx, typeof regEx);
+
+console.log("student instanceof Object):", student instanceof Object);
+console.log("studentNames instanceof Array:", studentNames instanceof Array);
+console.log("hello instanceof Function:", hello instanceof Function);
+console.log("hello instanceof Object:", hello instanceof Function);
+console.log("studentNames instanceof Object: ", hello instanceof Function);
+console.log("student instanceof Array:", hello instanceof Function);
