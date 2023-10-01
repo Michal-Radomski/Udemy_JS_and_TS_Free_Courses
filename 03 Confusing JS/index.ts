57ab724ce59a13c18bd9139aadb1d4c6e98d345f
Michal Radomski <michalra@op.pl>
2023-10-02 01:42:06 +0200

    course - commit 2

--- 03 Confusing JS/index.ts
+++ 03 Confusing JS/index.ts
@@ -1,21 +1,62 @@
 //* Scoping Chain
-var myName = "Mich";
-// console.log({ myName });
+// var myName = "Mich";
+// // console.log({ myName });
 
-(function first() {
-  const age = 30;
-  // console.log({ myName });
+// (function first() {
+//   const age = 30;
+//   // console.log({ myName });
 
-  if (age >= 21) {
-    // const isAllowedToDrive = true;
-    var isAnArmyLover = true;
-  }
-  // @ts-ignore
-  console.log({ isAnArmyLover });
+//   if (age >= 21) {
+//     // const isAllowedToDrive = true;
+//     var isAnArmyLover = true;
+//   }
+//   // @ts-ignore
+//   console.log({ isAnArmyLover });
 
-  function second() {
-    const job = "Software Engineer";
-    console.log(`${myName} ${age} ${job}`);
-  }
-  second();
-})();
+//   function second() {
+//     const job = "Software Engineer";
+//     console.log(`${myName} ${age} ${job}`);
+//   }
+//   second();
+// })();
+
+//* this keyword
+// console.log("this:", this);
+
+// (function calcAge(this: any) {
+//   console.log("this:", this);
+// })();
+
+const Mich = {
+  firstName: "Mich",
+  testing() {
+    console.log("this:", this);
+    // function innerFunction() {
+    //   console.log("this:", this);
+    // }
+    // innerFunction();
+  },
+};
+Mich.testing();
+
+var Mich2 = {
+  firstName: "Mich2",
+  testing() {
+    console.log("this:", this);
+  },
+};
+Mich2.testing();
+
+// const testing = () => {
+//   console.log("this:", this as typeof globalThis);
+// };
+// testing();
+
+// console.log(self);
+// console.log("globalThis:", globalThis);
+
+const Johny = {
+  firstName: "Johny",
+} as any;
+Johny.testing = Mich.testing;
+Johny.testing();
