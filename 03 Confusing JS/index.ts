362c6b48d67c7ee47a1503a680d7827d1dc9bdc1
Michal Radomski <michalra@op.pl>
2023-10-03 01:08:36 +0200

    course - commit 4

--- 03 Confusing JS/index.ts
+++ 03 Confusing JS/index.ts
@@ -73,10 +73,52 @@
 //   lastName: "Doe"
 // }
 
-const BluePrint = function (this: any, firstName: string, lastName: string) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-};
+// const BluePrint = function (this: any, firstName: string, lastName: string) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+// };
 
-const john = new (BluePrint as any)("John", "Doe");
-console.log({ john });
+// const john = new (BluePrint as any)("John", "Doe");
+// console.log({ john });
+
+//* Async JS
+// const listLocations = (locations: string[]) => locations.forEach((location: string) => console.log({ location }));
+// const myLocations = ["Olsztyn", "Gdansk", "Poznan"];
+// listLocations(myLocations);
+
+// console.log(1, "Starting");
+// setTimeout(() => {
+//   console.log(4, "Waiting 2s");
+// }, 2000);
+// setTimeout(() => {
+//   console.log(3, "Waiting 0s");
+// }, 0);
+// console.log(2, "Ending");
+
+//* Callback
+const x = function (str: string): void {
+  console.log("Function X");
+  console.log({ str });
+};
+const y = function (callback: Function, str: string): void {
+  console.log("Function Y");
+  callback(str);
+};
+y(x, "string");
+
+// const calc = function (num1: number, num2: number, calcType: string) {
+//   if (calcType === "add") {
+//     return num1 + num2;
+//   }
+//   if (calcType === "multiply") {
+//     return num1 * num2;
+//   }
+// };
+// console.log('calc(5, 5, "add"):', calc(5, 5, "add"));
+
+const add = (a: number, b: number) => a + b;
+const multiply = (a: number, b: number) => a * b;
+const calc = function (num1: number, num2: number, calcType: Function) {
+  return calcType(num1, num2);
+};
+console.log("calc(10,10, multiply):", calc(10, 10, multiply));
