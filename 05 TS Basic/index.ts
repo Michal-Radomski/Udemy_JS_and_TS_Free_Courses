7ca50748dcc73683c5b72ac04b42e33b7536b553
Michal Radomski <michalra@op.pl>
2023-12-13 02:20:45 +0100

    Course - commit 06 - course done

--- 05 TS Basic/index.ts
+++ 05 TS Basic/index.ts
@@ -84,83 +84,95 @@
 // console.log("permissions", permissions);
 
 //@ -> 3
-type UserPermissions = "admin" | "user" | "manager";
+// type UserPermissions = "admin" | "user" | "manager";
 
-type PermissionsWithoutAdmin = Exclude<UserPermissions, "admin">;
+// type PermissionsWithoutAdmin = Exclude<UserPermissions, "admin">;
 
-interface DepartmentsForPermission {
-  depName: string;
-  lvl: number;
-}
+// interface DepartmentsForPermission {
+//   depName: string;
+//   lvl: number;
+// }
 
-const DepsForPerms: Record<UserPermissions, DepartmentsForPermission> = {
-  admin: {
-    depName: "security",
-    lvl: 10,
-  },
-  user: {
-    depName: "sales",
-    lvl: 5,
-  },
-  manager: {
-    depName: "sales",
-    lvl: 10,
-  },
-};
+// const DepsForPerms: Record<UserPermissions, DepartmentsForPermission> = {
+//   admin: {
+//     depName: "security",
+//     lvl: 10,
+//   },
+//   user: {
+//     depName: "sales",
+//     lvl: 5,
+//   },
+//   manager: {
+//     depName: "sales",
+//     lvl: 10,
+//   },
+// };
 
-type TuplePermissions = [UserPermissions, UserPermissions];
+// type TuplePermissions = [UserPermissions, UserPermissions];
 
-type BasicUser<A = boolean, P = TuplePermissions> = {
-  // readonly name: string; //* You can't change this
+// type BasicUser<A = boolean, P = TuplePermissions> = {
+//   // readonly name: string; //* You can't change this
+//   name: string;
+//   surname: string;
+//   age: number;
+//   isAdmin: A;
+//   permissions?: P;
+// };
+
+// type BasicUserWithoutPermissions = Omit<BasicUser, "permissions">; //* without permissions
+
+// type AdvancedUser = {
+//   account: number;
+// };
+
+// type FullUser<A = boolean, P = string[]> = BasicUser<A, P> & AdvancedUser;
+
+// type BasicUserReadonly = Readonly<BasicUser>; //* every item is mark as readonly
+// type BasicUserRequired = Required<BasicUser>; //* every item is mark as required
+// type BasicUserPartial = Partial<BasicUser>; //* every item is mark as not required ( item? : string | undefined)
+
+// type BasicUserReadonlyRequired = Readonly<Required<BasicUser>>;
+
+// const user: FullUser<boolean> = {
+//   name: "Nick",
+//   surname: "Ovchinnikov",
+//   age: 30,
+//   isAdmin: true,
+//   account: 100,
+//   permissions: ["admin", "user"],
+// };
+
+// user.name = "Test"; //* can't be mark as readonly!
+
+// const usersArray: FullUser[] = [user, user];
+
+// function getFirst<T>(arr: T[]): T {
+//   return arr[0];
+// }
+
+// type BasicFunction = () => FullUser[];
+
+// type getFirstReturnType = ReturnType<BasicFunction>;
+
+// getFirst<FullUser>(usersArray);
+
+// type MathFunc<T = number> = (a: T, b: T) => T;
+
+// const mul: MathFunc = (a, b) => a * b;
+
+// const add: MathFunc = (a, b) => a + b;
+
+// console.log("mul(4,5):", mul(4, 5));
+// console.log("add(4,5):", add(4, 5));
+
+interface Person {
   name: string;
-  surname: string;
   age: number;
-  isAdmin: A;
-  permissions?: P;
-};
-
-type BasicUserWithoutPermissions = Omit<BasicUser, "permissions">; //* without permissions
-
-type AdvancedUser = {
-  account: number;
-};
-
-type FullUser<A = boolean, P = string[]> = BasicUser<A, P> & AdvancedUser;
-
-type BasicUserReadonly = Readonly<BasicUser>; //* every item is mark as readonly
-type BasicUserRequired = Required<BasicUser>; //* every item is mark as required
-type BasicUserPartial = Partial<BasicUser>; //* every item is mark as not required ( item? : string | undefined)
-
-type BasicUserReadonlyRequired = Readonly<Required<BasicUser>>;
-
-const user: FullUser<boolean> = {
-  name: "Nick",
-  surname: "Ovchinnikov",
-  age: 30,
-  isAdmin: true,
-  account: 100,
-  permissions: ["admin", "user"],
-};
-
-user.name = "Test"; //* can't be mark as readonly!
-
-const usersArray: FullUser[] = [user, user];
-
-function getFirst<T>(arr: T[]): T {
-  return arr[0];
+  location?: string;
 }
 
-type BasicFunction = () => FullUser[];
+const bob: Pick<Person, "name"> = {
+  name: "Bob", // `Pick` has only kept name, so age and location were removed from the type and they can't be defined here
+};
 
-type getFirstReturnType = ReturnType<BasicFunction>;
-
-getFirst<FullUser>(usersArray);
-
-type MathFunc<T = number> = (a: T, b: T) => T;
-
-const mul: MathFunc = (a, b) => a * b;
-
-const add: MathFunc = (a, b) => a + b;
-
-console.log("mul(4,5):", mul(4, 5));
-console.log("add(4,5):", add(4, 5));
+console.log({ bob });
